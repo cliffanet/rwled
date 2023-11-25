72ed64c984f4dce50be8ca285e102ba496dc745e
Cliff <>
2023-11-25 23:41:43 +0300

    rwdesign - загрузка json - объекты

--- rwdesign/lib/main.dart
+++ rwdesign/lib/main.dart
@@ -1,9 +1,9 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:rwdesign/elemfile.dart';
 import 'package:rwdesign/player.dart';
 import 'package:rwdesign/view/ctrl.dart';
-import 'package:rwdesign/view/title.dart';
 import 'package:rwdesign/painter.dart';
 
 void main() {
@@ -14,15 +14,17 @@ void main() {
     ));
 }
 
+enum MenuCode { Open }
+
 class DesignApp extends StatelessWidget {
     const DesignApp({super.key});
 
     // This widget is the root of your application.
     @override
     Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: PlaceTitle(),
-            body: Stack(children: [
+        return Material(
+            child: Stack(children: [
+                /*
                 ValueListenableBuilder(
                     valueListenable: Player().notify,
                     builder: (context, value, widget) {
@@ -43,6 +45,44 @@ class DesignApp extends StatelessWidget {
                             ),
                         );
                     }
+                ),
+                */
+                ValueListenableBuilder(
+                    valueListenable: ScenarioNotify,
+                    builder: (context, value, widget) {
+                        return CustomPaint(
+                            painter: PlacePainter(),
+                            size: Size.infinite,
+                        );
+                    }
+                ),
+                Positioned(
+                    right: 0,
+                    top: 0,
+                    child: PopupMenuButton<MenuCode>(
+                        tooltip: "Меню",
+                        onSelected: (MenuCode item) {
+                            switch (item) {
+                                case MenuCode.Open:
+                                    OpenScenarioDir();
+                                    break;
+                            };
+                        },
+                        itemBuilder: (context) {
+                            return [
+                                PopupMenuItem(
+                                    value: MenuCode.Open,
+                                    child: Row(
+                                        children: const [
+                                            Icon(Icons.folder, color: Colors.black),
+                                            SizedBox(width: 8),
+                                            Text('Открыть сценарий'),
+                                        ]
+                                    ),
+                                ),
+                            ];
+                        }
+                    )
                 ),
                 Positioned(
                     width: MediaQuery.of(context).size.width,
