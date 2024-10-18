83da41b2e33de4d0be925ecd5e731d541a7ca4c8
Cliff <>
2024-10-18 22:06:18 +0300

    описание параметров лент

---
+++ src/led/test.h
@@ -0,0 +1,16 @@
+/*
+    Led stream: test
+*/
+
+#ifndef _led_test_H
+#define _led_test_H
+
+#include "../../def.h"
+
+namespace LedTest {
+    void start();
+    void stop();
+};
+
+
+#endif // _led_test_H
